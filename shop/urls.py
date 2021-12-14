f4d603f7a64054dcb94e81aac842b8e7bf614adc
Visiteur RIF <loic.mahadzere.pro@gmail.com>
2021-12-14 15:06:28 +0100

    Ajout des liens des catégories

--- shop/urls.py
+++ shop/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
-from shop.views import index, detail, checkout, confirmation
+from shop.views import index, detail, checkout, confirmation, Boisson, Accompagnement, Hamburger, Dessert
 
 urlpatterns = [
     path('', index, name='home'),
-    path('', index, name='Hamburger'),
-    path('boissons', index, name='Boisson'),
-    path('', index, name='Accompagnement'),
-    path('', index, name='Dessert'),
+    path('Hamburger', Hamburger, name='Hamburger'),
+    path('Boisson', Boisson, name='Boisson'),
+    path('Accompagnement', Accompagnement, name='Accompagnement'),
+    path('Dessert', Dessert, name='Dessert'),
     path('<int:myid>', detail, name="detail"),
     path('checkout', checkout, name="checkout"),
     path('confirmation', confirmation, name="confirmation"),
--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,6 @@
 from django.core.checks.messages import Info
 from django.shortcuts import redirect, render
-from .models import Product, Commande
+from .models import Product, Commande, Category
 from django.core.paginator import Paginator
 
 # Create your views here.
@@ -8,6 +8,46 @@ def index(request):
     product_object = Product.objects.all()
     item_name = request.GET.get('item-name')
     if item_name !='' and item_name is not None:
+        product_object = Product.objects.filter(title__icontains=item_name)
+    paginator = Paginator(product_object, 4)
+    page = request.GET.get('page')
+    product_object = paginator.get_page(page)
+    return render(request, 'shop/index.html', {'product_object': product_object})
+
+def Hamburger(request):
+    product_object = Product.objects.filter(category=5)
+    item_name = request.GET.get('item-name')
+    if (item_name !=' ' and item_name is not None):
+        product_object = Product.objects.filter(title__icontains=item_name)
+    paginator = Paginator(product_object, 4)
+    page = request.GET.get('page')
+    product_object = paginator.get_page(page)
+    return render(request, 'shop/index.html', {'product_object': product_object})
+
+def Boisson(request):
+    product_object = Product.objects.filter(category=6)
+    item_name = request.GET.get('item-name')
+    if item_name !=' ' and item_name is not None:
+        product_object = Product.objects.filter(title__icontains=item_name)
+    paginator = Paginator(product_object, 4)
+    page = request.GET.get('page')
+    product_object = paginator.get_page(page)
+    return render(request, 'shop/index.html', {'product_object': product_object})
+
+def Dessert(request):
+    product_object = Product.objects.filter(category=7)
+    item_name = request.GET.get('item-name')
+    if item_name !=' ' and item_name is not None:
+        product_object = Product.objects.filter(title__icontains=item_name)
+    paginator = Paginator(product_object, 4)
+    page = request.GET.get('page')
+    product_object = paginator.get_page(page)
+    return render(request, 'shop/index.html', {'product_object': product_object})
+
+def Accompagnement(request):
+    product_object = Product.objects.filter(category=8)
+    item_name = request.GET.get('item-name')
+    if item_name !=' ' and item_name is not None:
         product_object = Product.objects.filter(title__icontains=item_name)
     paginator = Paginator(product_object, 4)
     page = request.GET.get('page')
